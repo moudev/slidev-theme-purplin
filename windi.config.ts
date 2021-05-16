e27cdeb90a7219c763f43d9e8b0d059ad1529c2d
moudev <rigobertomauriciomartinez@gmail.com>
2021-05-15 20:58:44 -0600

    style: update background, text color and font family

--- windi.config.ts
+++ windi.config.ts
@@ -13,13 +13,13 @@ export default mergeWindicssConfig(
     },
     shortcuts: {
       // custom the default background
-      'bg-main': 'bg-white text-[#181818] dark:(bg-[#121212] text-[#ddd])',
+      'bg-main': 'bg-purple-50 text-purple-900 dark:(bg-[#121212] text-purple-100)',
     },
     theme: {
       extend: {
         // fonts can be replaced here, remember to update the web font links in `index.html`
         fontFamily: {
-          sans: '"Nunito Sans", ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
+          sans: '"Rubik", ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
           mono: '"Fira Code", monospace',
         },
       },
