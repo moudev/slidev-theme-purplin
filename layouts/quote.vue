169793969dc9a72e6b27bdbb6a16ca020e4afdf2
moudev <rigobertomauriciomartinez@gmail.com>
2021-05-16 16:06:03 -0600

    feat: add text alignment prop in quote layout

--- layouts/quote.vue
+++ layouts/quote.vue
@@ -1,6 +1,27 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    position: {
+      type: String,
+      required: true,
+      default: 'left',
+    },
+  },
+  setup(props) {
+    const textAlignment = props.position
+
+    return {
+      textAlignment,
+    }
+  },
+})
+</script>
+
 <template>
   <div class="slidev-layout quote">
-    <div class="my-auto">
+    <div class="my-auto" :class="`text-${textAlignment}`">
       <slot />
     </div>
   </div>
